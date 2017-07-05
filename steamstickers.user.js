b977e553257ccca0fc9a5b0b95573f74fa04e435
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2017-07-05 16:19:01 +0545

    updated

--- steamstickers.user.js
+++ steamstickers.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Steam Stickers Helper
 // @namespace    https://github.com/ssovit/userscripts
-// @version      1.0.0
+// @version      1.1.0
 // @description  Sticker pasting helper for Lazy
 // @author       ssovit
 // @match        http://steamcommunity.com/id/*/stickers
@@ -13,16 +13,29 @@
 // ==/UserScript==
 (function () {
     'use strict';
-    $('#scene_selector').on('click', '.item', function () {
+    var scenes = $('#scene_selector .item');
+    var i = 0;
+    $('#scene_selector .item').on('click', 'img', function () {
         setTimeout(function () {
             var stickers = $('#sticker_selector .sticker_item');
             if (stickers.length < 16) {
                 stickers.trigger('click');
-                setTimeout(function () { g_StickerManager.SaveScene(false); }, 5 * 1000);
+                setTimeout(function () { g_StickerManager.SaveScene(false); }, 3 * 1000);
 
+            } else {
+                console.log('already completed');
             }
-        }, 5 * 1000);
+        }, 3 * 1000);
 
     });
-
+    var doScene = function () {
+        if (i < scenes.length) {
+            $('#scene_selector .item:eq(' + i + ') img').trigger('click');
+            setTimeout(function () {
+                i++;
+                doScene();
+            }, 10 * 1000);
+        }
+    };
+    doScene();
 })();
