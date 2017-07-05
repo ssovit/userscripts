14e2d7a5a66e5d00145377cc1e895b4ce9bdf4ed
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2017-07-05 17:40:14 +0545

    update

--- simplo.gg.user.js
+++ simplo.gg.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Simplo GG auto follow steps
 // @namespace    https://github.com/ssovit/userscripts
-// @version      1.0.1
+// @version      1.1.0
 // @description  Auto Bump for CSGoLounge.com
 // @author       ssovit
 // @match        https://simplo.gg/index.php?giveaway=*
@@ -11,19 +11,24 @@
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js
 // @run-at document-end
 // ==/UserScript==
-(function () {
+(function (_window) {
     'use strict';
-    var MyInterval = 2; // in seconds
-    var i = 0;
-    $('.mod-enter .enter-sec').each(function () {
-        var _parent = $(this).parent();
-        var _this = $(this);
-        setTimeout(function () {
-            _this.trigger('click');
-            $('.takeaction', _parent).click();
-        }, i * MyInterval * 1000);
-        i++;
+    _window.openInNewTab = function () {
+        console.log('no more new tab');
+    };
+    if ($('.sign-in .blur').length < 1) {
+        var MyInterval = 3; // in seconds
+        var i = 0;
+        $('.mod-enter .enter-sec').each(function () {
+            var _parent = $(this).parent();
+            var _this = $(this);
+            setTimeout(function () {
+                _this.trigger('click');
+                $('.takeaction', _parent).click();
+            }, i * MyInterval * 1000);
+            i++;
 
 
-    });
-})();
+        });
+    }
+})(window);
