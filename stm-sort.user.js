5043d185302eed2ff80bf6e043e4ff9f67669c13
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2017-08-05 17:22:57 +0545

    - will remove offline users and will try to keep trade bot matches on top

--- stm-sort.user.js
+++ stm-sort.user.js
@@ -2,7 +2,7 @@
 // @name        STM Auto Sort
 // @description STM Auto result sort so matches with more game cards will be put on top
 // @include     http://www.steamtradematcher.com/compare
-// @version     1.0.3
+// @version     1.0.5
 // @author      ssovit
 // @namespace   http://www.steamtradematcher.com
 // @icon        http://www.steamtradematcher.com/res/img/favicon.jpg
@@ -12,23 +12,29 @@
 // @run-at document-end
 // ==/UserScript==
 
-(function($){
+(function($) {
     'use strict';
-    var checkMatches=function(){
-        var panels=$('#match-results').children().detach();
-        panels.sort(function(a,b){
-            return $('.matches.filter-show',$(b)).length-$('.matches.filter-show',$(a)).length;
+    var checkMatches = function() {
+        $('#match-results').children().each(function() {
+            if (!$('.playerAvatar', $(this)).hasClass('online') && !$('.playerAvatar', $(this)).hasClass('in-game')) {
+                $(this).remove();
+                //$(this).css('opacity','0.2');
+            }
+        });
+        var panels = $('#match-results').children().detach();
+        panels.sort(function(a, b) {
+            return ($('.matches.filter-show', $(b)).length + $('.stm-user', $(b)).text().indexOf("Trade bot")) - ($('.matches.filter-show', $(a)).length + $('.stm-user', $(a)).text().indexOf("Trade bot"));
         });
         $('#match-results').append(panels);
 
-        if($('#progress-percent').html()==100){
+        if ($('#progress-percent').html() == 100) {
             //console.log("Everything Done");
             return;
         }
-        setTimeout(function(){
+        setTimeout(function() {
             //console.log("Still need to sort");
             checkMatches();
-        },3*1000);
+        }, 3 * 1000);
     };
     checkMatches();
 })(jQuery);
